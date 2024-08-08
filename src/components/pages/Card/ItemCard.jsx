e9bb36e1c258c6fdfae33d111a888638cb50e41b
devMachadoA <amachadoamaral2@gmail.com>
2024-08-08 12:31:41 +0100

    background-image

--- src/components/pages/Card/ItemCard.jsx
+++ src/components/pages/Card/ItemCard.jsx
@@ -12,16 +12,15 @@ export default function ItemCard({ item, activeLink }) {
   const { current, previous } = timeframes[activeLink];
 
   return (
-      <motion.id 
+      <motion.div 
         initial={{opacity: 0}}
         whileInView={{opacity: 1}}
         className={`${styles.sectionItem} ${styles[titleClass]}`}>
         <div className={`${styles[titleClass]} ${styles.headerItem} ${styles[`${titleClass}Img`]}`}>
-          {/* <img src={`./images/icon-${title.toLowerCase().replace(/ /g, '-')}.svg`} alt={`icon of the ${title.toLowerCase()}`} /> */}
         </div>
         <div className={styles.body}>
           <article className={styles.articleTitle}>
-            <h3>{title}</h3>
+            <h2>{title}</h2>
             <button aria-label='toggle menu'>
               <span className={styles.circle}></span>
               <span className={styles.circle}></span>
@@ -33,6 +32,6 @@ export default function ItemCard({ item, activeLink }) {
             <p>Last Week - {previous} hrs</p>
           </article>
         </div>
-      </motion.id>
+      </motion.div>
   );
 }
